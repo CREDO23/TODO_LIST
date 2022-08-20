1461bc2bd4e2797634f1309e12c043116a33dc0d
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-08-20 21:57:06 +0200

    feat : add formular to complete the tasks

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../Styles/taskForm.css'
+//import {AiOutlinePlusCircle} from 'react-icons/ai'
+
+
+const TaskForm = () => {
+  return (
+    <div className='container'>
+        <div className="form">
+            <div className="inputs">
+                <input type="text" placeholder='Title' className='input title' />
+                <input type="text" placeholder='Description' className='input description'/>
+            </div>
+            <div className="buttons">
+                <button className='button date'>Date</button>
+                <button className='button priority'>Priority</button>
+            </div>
+            <div className="add-task">
+                <button className='addtask-button'>
+                    Add a task
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default TaskForm
