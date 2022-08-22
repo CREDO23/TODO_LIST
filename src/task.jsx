e66b89f23cec1134709fbb47a9b0ef0415a7c936
CREDO23 <thierrybakera12@gmail.com>
2022-08-22 11:44:48 +0200

    feat : add TaskList tab

--- src/task.jsx
+++ src/task.jsx
@@ -64,5 +64,11 @@ const tasksList = [
         description : 'Begin to read the documentation of React , after design an build a real project .',
         date : 'Monday 22th March 2022',
         priority : 'hight'
+    },
+    {
+        title:'Learn React .',
+        description : 'Begin to read the documentation of React , after design an build a real project .',
+        date : 'Monday 22th March 2022',
+        priority : 'hight'
     }
 ]
