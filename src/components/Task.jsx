70dfed4782e75344890a6c565b6f1f1450999adc
cedric karungu <ckarungu921@gmail.com>
2022-08-22 12:23:13 +0200

    refact: update task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import { FaRegTrashAlt } from "react-icons/fa";
+import { FaRegTrashAlt } from 'react-icons/fa';
 
-const Task =() => {
-    return(
-        <div className='firstTaskdiv'>
-        <div className='titleAndDate'>
-       <h3 className='taskTitle'> Reviser React</h3>
-       <span className='dayTask'>Monday, 10h15</span>
-       </div>
-       <div className='descriptionAndDelete'>
-       <p> Dom virtuel, React Router, les props et States </p>
-       <span className='iconDelete'><FaRegTrashAlt/></span>
-        </div>
-        </div>
-    )
-}
-    
+const Task = () => {
+	return (
+		<div className='firstTaskdiv'>
+			<div className='titleAndDate'>
+				<h3 className='taskTitle'> Reviser React</h3>
+				<span className='dayTask'>Monday, 10h15</span>
+			</div>
+			<div className='descriptionAndDelete'>
+				<p> Dom virtuel, React Router, les props et States </p>
+				<span className='iconDelete'>
+					<FaRegTrashAlt />
+				</span>
+			</div>
+		</div>
+	);
+};
 
 export default Task;
