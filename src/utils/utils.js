f8b89d1c8bdb334a24704cfd52dcd1580dc6f2e5
CREDO23 <thierrybakera12@gmail.com>
2022-08-22 11:40:59 +0200

    feat : add TaskList_Tab

---
+++ src/utils/utils.js
@@ -0,0 +1,11 @@
+/** @format */
+const addTask = (title, description, date, priority) => {
+	return {
+		title,
+		description,
+		date,
+		priority,
+	};
+};
+
+export default addTask;
---
+++ task.jsx
@@ -0,0 +1,68 @@
+const tasksList = [
+    {
+        title:'Learn React .',
+        description : 'Begin to read the documentation of React , after design an build a real project .',
+        date : 'Monday 22th March 2022',
+        priority : 'hight'
+    },
+    {
+        title:'Learn React .',
+        description : 'Begin to read the documentation of React , after design an build a real project .',
+        date : 'Monday 22th March 2022',
+        priority : 'hight'
+    },
+    {
+        title:'Learn React .',
+        description : 'Begin to read the documentation of React , after design an build a real project .',
+        date : 'Monday 22th March 2022',
+        priority : 'hight'
+    },
+    {
+        title:'Learn React .',
+        description : 'Begin to read the documentation of React , after design an build a real project .',
+        date : 'Monday 22th March 2022',
+        priority : 'hight'
+    },
+    {
+        title:'Learn React .',
+        description : 'Begin to read the documentation of React , after design an build a real project .',
+        date : 'Monday 22th March 2022',
+        priority : 'hight'
+    },
+    {
+        title:'Learn React .',
+        description : 'Begin to read the documentation of React , after design an build a real project .',
+        date : 'Monday 22th March 2022',
+        priority : 'hight'
+    },
+    {
+        title:'Learn React .',
+        description : 'Begin to read the documentation of React , after design an build a real project .',
+        date : 'Monday 22th March 2022',
+        priority : 'hight'
+    },
+    {
+        title:'Learn React .',
+        description : 'Begin to read the documentation of React , after design an build a real project .',
+        date : 'Monday 22th March 2022',
+        priority : 'hight'
+    },
+    {
+        title:'Learn React .',
+        description : 'Begin to read the documentation of React , after design an build a real project .',
+        date : 'Monday 22th March 2022',
+        priority : 'hight'
+    },
+    {
+        title:'Learn React .',
+        description : 'Begin to read the documentation of React , after design an build a real project .',
+        date : 'Monday 22th March 2022',
+        priority : 'hight'
+    },
+    {
+        title:'Learn React .',
+        description : 'Begin to read the documentation of React , after design an build a real project .',
+        date : 'Monday 22th March 2022',
+        priority : 'hight'
+    }
+]
